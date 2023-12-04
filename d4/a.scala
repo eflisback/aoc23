5bc799d0f3c1a2e564308101ede605da0af6af38
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-12-04 20:41:47 +0100

    formatting

--- d4/a.scala
+++ d4/a.scala
@@ -4,13 +4,7 @@
   var sum = 0
 
   for line <- lines do
-    val wn = line
-      .split(':')(1)
-      .split('|')(0)
-      .split(' ')
-      .filter(_.nonEmpty)
-      .map(_.toInt)
-      .toSet
+    val wn = line.split(':')(1).split('|')(0).split(' ').filter(_.nonEmpty).map(_.toInt).toSet
     val yn = line.split('|')(1).split(' ').filter(_.nonEmpty).map(_.toInt).toSet
     val matches = for num <- yn if wn contains num yield num
     val toAdd = scala.math.pow(2, matches.size - 1).toInt
--- d4/b.scala
+++ d4/b.scala
@@ -2,25 +2,16 @@
   val lines = scala.io.Source.fromFile("./data.txt").getLines.toVector
 
   val matchesPerLine = lines.map(line =>
-    val wn = line
-      .split(':')(1)
-      .split('|')(0)
-      .split(' ')
-      .filter(_.nonEmpty)
-      .map(_.toInt)
-      .toSet
+    val wn = line.split(':')(1).split('|')(0).split(' ').filter(_.nonEmpty).map(_.toInt).toSet
     val yn = line.split('|')(1).split(' ').filter(_.nonEmpty).map(_.toInt).toSet
     val matches = yn.count(wn.contains)
     matches
   )
-
   var numOfCards = 0
-
   def getNbrScratchCardsFromLine(index: Int): Int =
     val matches = matchesPerLine(index)
     var toReturn = matches
     for i <- 1 to matches do toReturn += getNbrScratchCardsFromLine(index + i)
-
     toReturn
 
   for index <- matchesPerLine.indices do
