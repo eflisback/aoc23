f346ab8f0a53836a5e8610e0d7eefab06cd66381
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-12-07 16:32:25 +0100

    6b done

--- d6/b.scala
+++ d6/b.scala
@@ -1,34 +1,24 @@
-def getCurveIntersectionPointsX(duration: Long, highScore: Long): Vector[Long] = 
-    var pointsX: Vector[Long] = Vector.empty
-    val initPrecision = 100
-    var precision = initPrecision
-    var buttonHoldTime: Long = 0
+def getNumberOfWinOpportunities(duration: Long, highScore: Long): List[Long] =
+  var toReturn: List[Long] = List.empty
+  var buttonHoldTime: Long = 0
+  var previouslyAboveHighscore = false
+  while buttonHoldTime <= duration do
+    val traveledDistance = (duration - buttonHoldTime) * buttonHoldTime
+    val isAboveHighscore = traveledDistance > highScore
+    if isAboveHighscore && !previouslyAboveHighscore then
+      toReturn = toReturn :+ buttonHoldTime
+    else if !isAboveHighscore && previouslyAboveHighscore then
+      toReturn = toReturn :+ buttonHoldTime
+    previouslyAboveHighscore = isAboveHighscore
+    buttonHoldTime += 1
+  toReturn
 
-    var previouslyAboveHighscore = false
-    var passedFirstPoint = false
-    while buttonHoldTime < duration do
-        val traveledDistance = (duration - buttonHoldTime) * buttonHoldTime
-        var aboveHighscore = traveledDistance > highScore
-        // println(s"Travel distance is $traveledDistance.")
-        if aboveHighscore != previouslyAboveHighscore then
-            precision = -1 * (precision - 1)
-            // println(s"We've passed it. Changing precision to $precision...") 
-
-        if traveledDistance == highScore then 
-            println(s"Found point at $traveledDistance")
-            pointsX = pointsX :+ buttonHoldTime
-            precision = initPrecision
-            aboveHighscore = false
-            previouslyAboveHighscore = false
-
-        previouslyAboveHighscore = aboveHighscore
-        buttonHoldTime += precision
-    pointsX
-
-@main def run = 
-    val lines = scala.io.Source.fromFile("./data.txt").getLines.toVector
-    val duration = lines(0).drop(5).filterNot(_.isWhitespace).toLong
-    val highscore = lines(1).drop(9).filterNot(_.isWhitespace).toLong
-    val race = (duration, highscore)
-    val intersectionPointsX = getCurveIntersectionPointsX(duration, highscore)
-    println(intersectionPointsX(1) - intersectionPointsX(0))
+@main def run =
+  val lines = scala.io.Source.fromFile("./data.txt").getLines.toVector
+  val duration = lines(0).drop(5).filterNot(_.isWhitespace).toLong
+  val highscore = lines(1).drop(9).filterNot(_.isWhitespace).toLong
+  val race = (duration, highscore)
+  val winOpportunityPoints = getNumberOfWinOpportunities(duration, highscore)
+  val differences =
+    winOpportunityPoints.grouped(2).map(pair => pair(1) - pair(0))
+  println(differences.mkString)
